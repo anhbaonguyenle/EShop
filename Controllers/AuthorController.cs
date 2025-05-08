722bbc5487191793484e69f7a71f6699aab75124
Lê Nguyễn Bảo Anh <anhlenguyenbaoanh@gmail.com>
2025-05-08 23:06:03 +0700

    Author page

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,39 @@
+﻿using EShop.Models;
+using EShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly EShopContext db;
+        public AuthorController(EShopContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            var query = db.AuthorModels.AsQueryable();
+            if (id.HasValue)
+            {
+                query = query.Where(p => p.AuthorId == id.Value);
+            }
+
+            var authors = query.Select(p => new AuthorViewModel
+            {
+                AuthorId = p.AuthorId,
+                AuthorName = p.AuthorName,
+                AuthorNation = p.AuthorNation,
+                AuthorDescription = p.AuthorDescription,
+                AuthorImage = p.AuthorImage,
+            }).ToList();
+
+            return View(authors);
+        }
+
+    }
+}
---
+++ ViewModels/AuthorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace EShop.ViewModels
+{
+    public class AuthorViewModel
+    {
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorNation { get; set; }
+        public string? AuthorDescription { get; set; }
+        public string? AuthorImage { get; set; }
+    }
+}
